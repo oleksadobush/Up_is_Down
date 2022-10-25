a4aa7ae12e1cd7084b8a18097c8b396c94dcc8e0
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2022-10-25 20:01:48 +0300

    add player that runs, jumps and can run faster

---
+++ Scenes/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export var speed = 600
+export var jumpforce = 800
+export var gravity = 2000
+export var speed_up = 700
+
+var jumped = false
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.x = speed
+	if Input.is_action_just_pressed("UP"):
+		if not jumped:
+			velocity.y -= jumpforce
+			jumped = true
+	if Input.is_action_pressed("RIGHT"):
+		velocity.x += speed_up
+	if Input.is_action_pressed("DOWN"):
+		pass
+	velocity.y += gravity*delta
+	velocity.y = move_and_slide(velocity, Vector2.UP).y
+	
+	if is_on_floor():
+		jumped = false
+	else:
+		jumped = true
+	

abd9514df41acb82d0ead71690c72c8f1de84a96
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2022-10-31 21:22:06 +0200

    fix tile to fall

--- Scenes/InteractiveFloor/TileToFall.gd
+++ Scenes/InteractiveFloor/TileToFall.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+var falling_speed = 10000
+
 func _ready():
 	$Sprite/AnimationPlayer.current_animation = "default"
 
 func _on_MovingTile_body_entered(body):
 	$Sprite/AnimationPlayer.current_animation = "falling"
-	body.velocity.y += 500
+	body.gravity += falling_speed

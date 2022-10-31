fb4e7bf3e349353eac186295f7473104c46d0d01
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2022-10-31 20:58:49 +0200

    add edge handling

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,32 +6,32 @@ export var springforce = 6000
 export var gravity = 2000
 export var speed_up = 600
 
-onready var ray = get_node("EdgeRay")
+onready var ray_fall = get_node("EdgeRayFall")
+onready var ray_jump = get_node("EdgeRayJump")
+onready var ray_wall = get_node("WallRay")
 
 var jumped = false
 var sliding = false
 var velocity = Vector2()
-onready var animation_player = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	velocity.x = speed
 	if Input.is_action_just_pressed("UP"):
 		if not jumped:
-			if ray.is_colliding():
-				print(ray.position)
 			velocity.y -= jumpforce
+			if not ray_wall.is_colliding():
+				if ray_fall.is_colliding():
+					velocity.y += 200
+				elif ray_jump.is_colliding():
+					velocity.y -= 200
 			jumped = true
 
 	if Input.is_action_pressed("RIGHT"):
 		if not jumped:
 			velocity.x += speed_up
 
-	if Input.is_action_just_pressed("DOWN"):
-		animation_player.play("start_slide")
-		sliding = true
-	if Input.is_action_just_released("DOWN"):
-		animation_player.play("finish_slide")
-		sliding = false
+	if Input.is_action_pressed("DOWN"):
+		pass
 	
 	velocity.y += gravity*delta
 	velocity.y = move_and_slide(velocity, Vector2.UP).y

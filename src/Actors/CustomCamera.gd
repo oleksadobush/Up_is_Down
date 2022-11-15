7c8c26c0138d123589e7e73c64133b746bd2b2ad
Kolotose <amazourick@gmail.com>
2022-11-15 16:36:08 +0200

    Changed some dependencies and numbers replaced with constants

--- src/Actors/CustomCamera.gd
+++ src/Actors/CustomCamera.gd
@@ -2,19 +2,21 @@ extends Camera2D
 
 var DIFFERENCE = 512 # 512
 var SHIFT = 768 # 1280
+var X_OFFSET = 300
+var Y_OFFSET = 250
 
 var next_position: Vector2 = Vector2()
 
 onready var player = get_parent().get_node("Player")
 
 func _ready():
-	next_position.x = player.get_position().x + 300
-	next_position.y = player.get_position().y - 250
+	next_position.x = player.get_position().x + X_OFFSET
+	next_position.y = player.get_position().y - Y_OFFSET
 	set_position(next_position)
 
 
 func _physics_process(delta):
-	next_position.x = player.get_position().x + 300
+	next_position.x = player.get_position().x + X_OFFSET
 	if player.get_position().y >= get_position().y + DIFFERENCE:
 		next_position.y += SHIFT
 	elif player.get_position().y <= get_position().y - DIFFERENCE:

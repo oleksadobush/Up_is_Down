e5efb3da03363dbda432e4d85f50c88dd20825e7
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2022-11-15 09:28:42 +0200

    add enemy that crashes game

---
+++ src/Enemies/Enemy.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const ENEMY_SPEED: int = 600
+const GRAVITY: int = 2000
+const JUMP_FORCE: int = 775
+const LERP_WEIGHT: float = 0.3
+const SPRING_FORCE: int = 6000
+
+onready var Player = get_node("../Player")
+
+onready var EdgeRayJump = get_node("EdgeRayJump")
+onready var WallRay = get_node("WallRay")
+onready var CeillingRay = get_node("CeillingRay")
+onready var AnimPlayer = get_node("AnimationPlayer")
+
+var sliding: bool = false
+var jumped: bool = false
+var velocity: Vector2 = Vector2()
+
+func _physics_process(delta):
+	if Player:
+		velocity.x = ENEMY_SPEED
+		var direction = (Player.position - position).normalized()
+		
+		if EdgeRayJump.is_colliding() and not sliding:
+			AnimPlayer.play("start_slide")
+			sliding = true
+		if sliding and not CeillingRay.is_colliding() and not EdgeRayJump.is_colliding():
+			AnimPlayer.play("finish_slide")
+			sliding = false
+		if not sliding and not jumped and WallRay.is_colliding():
+			velocity.y -= JUMP_FORCE
+			jumped = true
+		
+		velocity.y = move_and_slide(direction * ENEMY_SPEED + Vector2.DOWN * GRAVITY * delta).y
+
+func spring_jump():
+	velocity.y -= lerp(velocity.y, velocity.y + SPRING_FORCE, LERP_WEIGHT)
+
+
+func _on_Area2D_body_entered(body):
+	body.queue_free()
+
+

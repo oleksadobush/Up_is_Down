a5242932515f933d94ab751e06e6135301549777
Kolotose <amazourick@gmail.com>
2023-03-03 17:35:32 +0200

    Commit of level and other global changes

--- src/Actors/CustomCamera.gd
+++ src/Actors/CustomCamera.gd
@@ -1,9 +1,17 @@
 extends Camera2D
 
-var DIFFERENCE = 512 # 512
-var SHIFT = 768 # 1280
-var X_OFFSET = 300
-var Y_OFFSET = 250
+# 768 black px
+# bteween levels 384 white px
+# one lewel is 1152 px in height
+
+var DIFFERENCE = 576 # 512
+var SHIFT = 768 + 384 - 32 # 1152
+var X_OFFSET = 352
+var Y_OFFSET = 352
+
+var LEVEL1_Y = 384
+var LEVEL2_Y = 384 + 1152
+var LEVEL3_Y = 384 + 1152*2
 
 var next_position: Vector2 = Vector2()
 
